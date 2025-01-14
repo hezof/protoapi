6ffee6151270884876bd8756c9d4249deebcc51f
hezhaowu <hezhaowu@qq.com>
2025-01-15 00:45:00 +0800

    save

--- cmd/protoc-gen-go-protoapi/docs.go
+++ cmd/protoc-gen-go-protoapi/docs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"google.golang.org/protobuf/compiler/protogen"
+	"gopkg.in/yaml.v2"
 )
 
 // generateDocsFile 生成文档文件: *_json
@@ -16,10 +17,10 @@ func generateDocsFile(gen *protogen.Plugin, file *protogen.File, meta *FileExt) 
 }
 
 type OASv2Doc struct {
-	Swagger     string                  `yaml:"swagger,omitempty"`
-	Tags        []*OASv2Tag             `yaml:"tags,omitempty"`
-	Paths       map[string]*OASv2Path   `yaml:"paths,omitempty"`
-	Definitions map[string]*OASv2Schema `yaml:"definitions,omitempty"`
+	Swagger     string        `yaml:"swagger,omitempty"`
+	Tags        []*OASv2Tag   `yaml:"tags,omitempty"`
+	Paths       yaml.MapSlice `yaml:"paths,omitempty"`       // OASv2Path
+	Definitions yaml.MapSlice `yaml:"definitions,omitempty"` //OASv2Schema
 }
 
 type OASv2Tag struct {
@@ -29,6 +30,7 @@ type OASv2Tag struct {
 }
 
 type OASv2Path struct {
+	XOrder  int             `yaml:"x-order,omitempty"`
 	Get     *OASv2Operation `json:"get,omitempty"`
 	Put     *OASv2Operation `json:"put,omitempty"`
 	Post    *OASv2Operation `json:"post,omitempty"`
@@ -39,14 +41,14 @@ type OASv2Path struct {
 }
 
 type OASv2Operation struct {
-	XOrder      int                       `yaml:"x-order,omitempty"`
-	OperationId string                    `yaml:"operationId,omitempty"`
-	Summary     string                    `yaml:"summary,omitempty"`
-	Description string                    `yaml:"description,omitempty"`
-	Tags        []string                  `yaml:"tags,omitempty"`
-	Parameters  []*OASv2Parameter         `yaml:"parameters,omitempty"`
-	Responses   map[string]*OASv2Response `yaml:"responses,omitempty"`
-	Deprecated  bool                      `yaml:"deprecated,omitempty"`
+	XOrder      int               `yaml:"x-order,omitempty"`
+	OperationId string            `yaml:"operationId,omitempty"`
+	Summary     string            `yaml:"summary,omitempty"`
+	Description string            `yaml:"description,omitempty"`
+	Tags        []string          `yaml:"tags,omitempty"`
+	Parameters  []*OASv2Parameter `yaml:"parameters,omitempty"`
+	Responses   yaml.MapSlice     `yaml:"responses,omitempty"` // OASv2Response
+	Deprecated  bool              `yaml:"deprecated,omitempty"`
 }
 
 type OASv2Parameter struct {
@@ -66,23 +68,23 @@ type OASv2Response struct {
 }
 
 type OASv2Schema struct {
-	XOrder               int                     `yaml:"x-order,omitempty"`
-	Ref                  string                  `yaml:"$ref,omitempty"`
-	Type                 string                  `yaml:"type,omitempty"`
-	Format               string                  `yaml:"format,omitempty"`               // 当type为scalar类型时描述格式
-	Items                *OASv2Schema            `yaml:"items,omitempty"`                // 当type为array时描述元素
-	AdditionalProperties *OASv2Schema            `yaml:"additionalProperties,omitempty"` // 当type为object时描述元素类型(map)
-	Properties           map[string]*OASv2Schema `yaml:"properties,omitempty"`           // 当type为object时描述属性
-	Required             []string                `yaml:"required,omitempty"`
-	Maximum              *float64                `json:"maximum,omitempty"`
-	ExclusiveMaximum     bool                    `json:"exclusiveMaximum,omitempty"`
-	Minimum              *float64                `json:"minimum,omitempty"`
-	ExclusiveMinimum     bool                    `json:"exclusiveMinimum,omitempty"`
-	MaxLength            *int64                  `json:"maxLength,omitempty"`
-	MinLength            *int64                  `json:"minLength,omitempty"`
-	Pattern              string                  `json:"pattern,omitempty"`
-	MaxItems             *int64                  `json:"maxItems,omitempty"`
-	MinItems             *int64                  `json:"minItems,omitempty"`
-	Enum                 []interface{}           `json:"enum,omitempty"`
-	Deprecated           bool                    `yaml:"deprecated,omitempty"`
+	XOrder               int           `yaml:"x-order,omitempty"`
+	Ref                  string        `yaml:"$ref,omitempty"`
+	Type                 string        `yaml:"type,omitempty"`
+	Format               string        `yaml:"format,omitempty"`               // 当type为scalar类型时描述格式
+	Items                *OASv2Schema  `yaml:"items,omitempty"`                // 当type为array时描述元素
+	AdditionalProperties *OASv2Schema  `yaml:"additionalProperties,omitempty"` // 当type为object时描述元素类型(map)
+	Properties           yaml.MapSlice `yaml:"properties,omitempty"`           // OASv2Schema,当type为object时描述属性
+	Required             []string      `yaml:"required,omitempty"`
+	Maximum              *float64      `json:"maximum,omitempty"`
+	ExclusiveMaximum     bool          `json:"exclusiveMaximum,omitempty"`
+	Minimum              *float64      `json:"minimum,omitempty"`
+	ExclusiveMinimum     bool          `json:"exclusiveMinimum,omitempty"`
+	MaxLength            *int64        `json:"maxLength,omitempty"`
+	MinLength            *int64        `json:"minLength,omitempty"`
+	Pattern              string        `json:"pattern,omitempty"`
+	MaxItems             *int64        `json:"maxItems,omitempty"`
+	MinItems             *int64        `json:"minItems,omitempty"`
+	Enum                 []interface{} `json:"enum,omitempty"`
+	Deprecated           bool          `yaml:"deprecated,omitempty"`
 }
--- cmd/protoc-gen-go-protoapi/go.mod
+++ cmd/protoc-gen-go-protoapi/go.mod
@@ -1,3 +1,7 @@
 module github.com/hezof/protoapi/cmd/protoc-gen-go-protoapi
 
 go 1.21
+
+require (
+	gopkg.in/yaml.v2 latest
+)
