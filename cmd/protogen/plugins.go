e25f995004f14305787750bd8589bf95471f40b5
hezhaowu <hezhaowu@qq.com>
2025-01-14 12:25:55 +0800

    save

--- cmd/protogen/plugins.go
+++ cmd/protogen/plugins.go
@@ -13,7 +13,7 @@ var Plugins = []*Plugin{
 		Mode:    GoGetSrc,
 		Name:    "include",
 		Module:  "github.com/hezof/protoapi/cmd/include",
-		Version: "v0.5.0",
+		Version: "v0.5.1",
 	},
 	{
 		Mode:    GoGetBin,
